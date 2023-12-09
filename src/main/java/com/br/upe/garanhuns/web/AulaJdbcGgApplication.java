9cf5a815ba69c9d3ea2e4c896100589a13a64330
Guilherme Rivaldy <Guilhermerivaldy288@hotmail.com>
2023-12-09 16:22:44 -0300

    first commit

---
+++ src/main/java/com/br/upe/garanhuns/web/AulaJdbcGgApplication.java
@@ -0,0 +1,13 @@
+package com.br.upe.garanhuns.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AulaJdbcGgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AulaJdbcGgApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/br/upe/garanhuns/web/UsuarioRepositorio.java
@@ -0,0 +1,9 @@
+package com.br.upe.garanhuns.web;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UsuarioRepositorio extends CrudRepository<Usuario, Long>{
+
+}
